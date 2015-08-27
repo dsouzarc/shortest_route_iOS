ce7966393894ce70c3e412474ba2f4e91858db83
Ryan D'souza <dsouzarc@gmail.com>
2015-08-27 02:09:20 -0400

    Added viewcontroller for adding destinations

---
+++ Quickest Route/DestinationsViewController.h
@@ -0,0 +1,13 @@
+//
+//  DestinationsViewController.h
+//  Quickest Route
+//
+//  Created by Ryan D'souza on 8/26/15.
+//  Copyright © 2015 Ryan D'souza. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DestinationsViewController : UIViewController
+
+@end
