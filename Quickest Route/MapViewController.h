6e456afa74ab7719fd6bdd1cbce49a63f0f7fa74
Ryan D'souza <dsouzarc@gmail.com>
2015-08-28 02:09:53 -0400

    Added mapviewcontroller for viewing destinations on the map

---
+++ Quickest Route/MapViewController.h
@@ -0,0 +1,13 @@
+//
+//  MapViewController.h
+//  Quickest Route
+//
+//  Created by Ryan D'souza on 8/28/15.
+//  Copyright © 2015 Ryan D'souza. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MapViewController : UIViewController
+
+@end
